a23b95e9f8a82ca6b51cb58423b47cffd81d08f2
squach28 <squach3443@gmail.com>
2023-07-24 15:38:08 -0700

    basic post page ui

--- client/src/pages/CreatePostPage.jsx
+++ client/src/pages/CreatePostPage.jsx
@@ -1,7 +1,37 @@
+import { useState } from "react"
+import Navbar from "../components/Navbar"
+
 const CreatePostPage = () => {
-  return (
-    <div>CreatePostPage</div>
-  )
+    const [title, setTitle] = useState('')
+    const [content, setContent] = useState('')
+
+    const postCopypasta = (e) => {
+        e.preventDefault()
+    }
+
+    return (
+        <div>
+            <Navbar />
+            <div className="flex flex-col m-10">
+                <h1 className="text-3xl">Post Your Copypasta</h1>
+                <form className="flex flex-col">
+                    <label htmlFor="title">Title</label>
+                    <input 
+                        id="title"
+                        type="text"
+                        placeholder="Your Cool Title"
+                        onChange={(e) => setTitle(e.target.value)}
+                    />
+                    <label htmlFor="content">Content</label>
+                    <textarea
+                        rows="13"
+                        className="border-2 resize-none" 
+                     />
+                     <button className="border-2 p-2 mt-3 cursor-pointer" onClick={postCopypasta}>Post</button>
+                </form>
+            </div>
+        </div>
+    )
 }
 
 export default CreatePostPage
